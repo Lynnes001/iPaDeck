bc83b965e9f46036c431c7ffdfe9ce4c197202b4
Lynnes <sea.yang@hotmai.com>
2022-05-17 23:58:49 +0800

    initi applications

---
+++ src/components/Sticker.js
@@ -0,0 +1,18 @@
+import "../css/Sticker.css"
+
+
+function Sticker() {
+    return (
+        <div className="sticker">
+
+            <p className="title">Temperature Control</p>
+            <p className="sub-title">Set to 10C</p>
+
+        </div>
+    );
+
+}
+
+
+
+export default Sticker;
---
+++ src/components/StickerContainer.js
@@ -0,0 +1,24 @@
+import '../css/StickerContainer.css';
+import Sticker from './Sticker';
+
+function StickerContainer() {
+  return (
+    <div className="sticker-container">
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+      <Sticker className="sticker" /> 
+    </div>    
+  );
+}
+
+
+export default StickerContainer;
